33b2235ef8b562dc1d27c281255b08fd1d7c13f6
rpg3d <imxqliu@gmail.com>
2020-04-09 21:22:07 +0800

    1.add LUA source

---
+++ Source/ThirdParty/LuaSource/Private/LuaSource.cpp
@@ -0,0 +1,6 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LuaSource.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_GAME_MODULE(FDefaultGameModuleImpl, Lua);
