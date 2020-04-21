36c143970865f16a4f1c08774eaa8c4b4fe185b8
rpd3d <imxqliu@live.com>
2020-04-21 18:38:44 +0800

    1.change LUA dir

---
+++ Content/LuaScript/ApplicationMain.lua
@@ -0,0 +1,28 @@
+--this file is the entry file for LuaScript plugin(UE4)
+
+
+Timer = Timer or require('Timer')
+
+G_Timer = Timer:new('GTimer')
+
+function LuaTick(InDeltaTime)
+	G_Timer:Tick(InDeltaTime)
+end
+
+function Main()
+	print(("----Lua Ram: %.2fMB----"):format(collectgarbage("count") / 1024))
+	G_Timer:SetTimer('MainDelayInit', 1, 0.1, DelayInit, nil)
+end
+
+function DelayInit()
+	local co = coroutine.create(
+		function()
+			KismetSystemLibrary:Delay(GameInstance, 2.0)
+			print(222)
+		end
+	)
+	coroutine.resume(co)
+	print(111)
+end
+
+
